684b15afa719da7e4fabf02ab2e87ae12171cce3
SakshiNikhade <sakshi.nikhade013@gmail.com>
2025-09-04 14:04:38 +0530

    Basic Addition

---
+++ CoreJavaPractice/src/addition/com/Addition.java
@@ -0,0 +1,17 @@
+package addition.com;
+
+public class Addition {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int a=10;
+		int b=20;
+		int c=20;
+		int d=30;
+		int addition=a+b+c+d;
+		System.out.println(addition);
+		System.out.println(addition);
+	}
+
+}
